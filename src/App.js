efa5a744e05ff2a3841ef2507dbf480d2856e7db
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-08-10 17:14:23 -0300

    Criando o card colaboradores do projeto

--- src/App.js
+++ src/App.js
@@ -225,10 +225,18 @@ function App() {
     
   }
 
+  function cadastrarTime (novoTime) {
+    setTimes([ ...times, { ...novoTime, id: uuidv4() } ])
+  }
+
   return (
     <div>
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <Formulario 
+        cadastrarTime={cadastrarTime}
+        times={times.map(time => time.nome)} 
+        aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} 
+      />
       <section className="times">
         <h1>Minha organização</h1>
         {times.map((time, indice) => 
